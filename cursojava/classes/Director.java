3109f1b6c868394e0822b45d89d52056ec9ff053
renanrcs <renan.cavalcante@yahoo.com>
2022-08-26 14:52:10 -0300

    creating class director and set atributes

---
+++ cursojava/classes/Director.java
@@ -0,0 +1,28 @@
+package cursojava.classes;
+
+public class Director extends Person{
+	
+	private String educationRegister;
+	private int directionTime;
+	private String titration;
+	
+	public String getEducationRegister() {
+		return educationRegister;
+	}
+	public void setEducationRegister(String educationRegister) {
+		this.educationRegister = educationRegister;
+	}
+	public int getDirectionTime() {
+		return directionTime;
+	}
+	public void setDirectionTime(int directionTime) {
+		this.directionTime = directionTime;
+	}
+	public String getTitration() {
+		return titration;
+	}
+	public void setTitration(String titration) {
+		this.titration = titration;
+	}
+
+}
