67cb5e1772f73ecd97bed2a650180d4730d92bd7
renanrcs <renan.cavalcante@yahoo.com>
2022-08-26 15:00:03 -0300

    creating class Secretary y appling heritage of person and seting atributes

---
+++ cursojava/classes/Secretary.java
@@ -0,0 +1,28 @@
+package cursojava.classes;
+
+public class Secretary extends Person {
+	
+	private String register;
+	private String jobLevel;
+	private String experience;
+	
+	public String getRegister() {
+		return register;
+	}
+	public void setRegister(String register) {
+		this.register = register;
+	}
+	public String getJobLevel() {
+		return jobLevel;
+	}
+	public void setJobLevel(String jobLevel) {
+		this.jobLevel = jobLevel;
+	}
+	public String getExperience() {
+		return experience;
+	}
+	public void setExperience(String experience) {
+		this.experience = experience;
+	}
+
+}
